767b6df039d333f9bdd32ac9d322d7b40c9335cf
RSLilova18 <RSLilova18@codingburgas.bg>
2022-11-14 11:42:45 +0200

    Initial commit

---
+++ src/main/java/com/example/vacationmanager/User.java
@@ -0,0 +1,12 @@
+package com.example.vacationmanager;
+
+public class User {
+
+    private String username;
+    private String name;
+
+    private String lastName;
+    private String role;
+    private String team;
+
+}
