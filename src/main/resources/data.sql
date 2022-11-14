b3850ec27a63abecf87d38ea23961eb8f92449d6
RSLilova18 <RSLilova18@codingburgas.bg>
2022-11-14 14:04:51 +0200

    database insert data update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO Roles(id, role_name)
 VALUES (1, 'CEO');
+INSERT INTO Roles(id, role_name)
+VALUES (2, 'Developer');
+INSERT INTO Roles(id, role_name)
+VALUES (3, 'Team Lead');
+INSERT INTO Roles(id, role_name)
+VALUES (4, 'Unassigned');
